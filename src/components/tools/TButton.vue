4e8875d5565a0f55d4b417bb3fe8c5e5972b4d4e
Maxime Valette <valettemaxime26@gmail.com>
2021-06-01 18:01:35 +0200

    ajout de la navigation + initialisation page home

---
+++ src/components/tools/TButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button
+        class="button"
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "TButton",
+    props: {
+        label: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+.button {
+    height: 25px;
+    width: fit-content;
+    display: flex;
+    background-color: #00F5D4;
+    color: black;
+    justify-content: center;
+    align-items: center;
+    font-size: inherit;
+    font-family: inherit;
+    padding: 4px;
+    border: 0;
+    border-radius: 10px;
+}
+</style>
