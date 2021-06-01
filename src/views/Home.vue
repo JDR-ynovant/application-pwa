b2cd04137fbc761624ba0d3f528e1be4d07b03cf
mguerrero <marianne.guerrero5@orange.fr>
2021-06-01 15:05:19 +0200

    add component organisation : grid & remove helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Grid></Grid>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Grid from "@/components/Grid.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Grid,
   },
 };
 </script>
