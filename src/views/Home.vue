54e52492daaa7ddc1e0efb3353aa1bc52e17faa4
Maxime Valette <valettemaxime26@gmail.com>
2021-06-01 18:01:35 +0200

    ajout de la navigation + initialisation page home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="home">
-    <Grid></Grid>
+  <div id="page-home">
+    <img src="../assets/img/licorne.png" />
+    <t-button 
+      label="Create a game"
+      @click=""
+    />
+    <span>Or ...</span>
+    <t-button 
+      label="Join a game"
+      @click=""
+    />
+    <img src="../assets/img/heart.png" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Grid from "@/components/Grid.vue";
+import TButton from "@/components/tools/TButton.vue";
 
 export default {
   name: "Home",
   components: {
-    Grid,
-  },
+    TButton
+  }
 };
 </script>
+<style scoped>
+  #page-home {
+    background-color: #FFB0B0;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
