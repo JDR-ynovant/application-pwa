1e3ef6336816145b5aed1071a36cd52f63ec884c
Maxime Valette <valettemaxime26@gmail.com>
2021-06-01 15:38:54 +0200

    initialisation du service worker

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   lintOnSave: false,
+  pwa: {
+    name: "JDR Ynovant",
+    assetsVersion: "0.1",
+    themeColor: "#4DBA87",
+    appleMobileWebAppCapable: "yes",
+    iconPaths: {
+      favicon32: '',
+      favicon16: '',
+      appleTouchIcon: '',
+      maskIcon: '',
+      msTileImage: ''
+    },
+    // configuration de workbox
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: 'src/workers/service-worker.js'
+    }
+  }
 };
