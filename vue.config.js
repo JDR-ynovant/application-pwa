055b6d08fc0ef901f521d4ccca9d9f1ad9d16d2c
Maxime Valette <valettemaxime26@gmail.com>
2021-06-02 11:57:40 +0200

    fix image + affichage des cellules et de la grille

--- vue.config.js
+++ vue.config.js
@@ -7,11 +7,11 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     iconsPath: {
       faviconSVG: './favicon.ico',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon.png',
-      maskIcon: 'img/icons/android-chrome-192x192.png',
-      msTileImage: 'img/icons/android-chrome-192x192.png'
+      favicon32: 'assets/icons/favicon-32x32.png',
+      favicon16: 'assets/icons/favicon-16x16.png',
+      appleTouchIcon: 'assets/icons/apple-touch-icon.png',
+      maskIcon: 'assets/icons/android-chrome-192x192.png',
+      msTileImage: 'assets/icons/android-chrome-192x192.png'
     },
     // configuration de workbox
     workboxPluginMode: "InjectManifest",
