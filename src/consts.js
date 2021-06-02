8ea52fbecb79738fc81f8bf61d9311151aae66c0
Maxime Valette <valettemaxime26@gmail.com>
2021-06-02 10:53:56 +0200

    ajout du statut des cellules

---
+++ src/consts.js
@@ -0,0 +1,8 @@
+export default {
+    cellStatus: {
+        VIDE: "VIDE",
+        OBSTACLE: "OBSTACLE",
+        JOUEUR: "JOUEUR",
+        OBJET: "OBJET",
+    }
+}
---
+++ src/mixins/GridMixins.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    computed: {
+        cssGridVariables(){
+            return {
+                "--nbCols" : this.nbCols,
+                "--nbRows" : this.nbRows
+            }
+        }
+    },
+}
+</script>
