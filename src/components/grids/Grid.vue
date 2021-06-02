7ae8be4d2a9d5cff5214ad8a89ea6cb232667865
Maxime Valette <valettemaxime26@gmail.com>
2021-06-02 10:14:57 +0200

    Modification des lien vers les icons

--- src/components/grids/Grid.vue
+++ src/components/grids/Grid.vue
@@ -12,11 +12,18 @@ export default {
   components: {
     Cell,
   },
+  props: {
+    nbCols : {
+      type: Number,
+      required: true
+    },
+    nbRows: {
+      type: Number,
+      required: true
+    }
+  },
   data: () => {
-    return {
-      nbCols: 20,
-      nbRows: 10,
-    };
+    return {};
   },
   computed: {
     layout() {
