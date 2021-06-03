8fb3f2affc1e715e596405f46317e8448ede9829
mguerrero <marianne.guerrero5@orange.fr>
2021-06-03 17:33:11 +0200

    create game form

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,29 +1,105 @@
 <template>
   <div class="center-content">
-    <img src="../assets/img/licorne.png" class="m-10"/>
-    <t-button
-        label="Add a player"
-        class="m-10"
-    />
+    <img src="../assets/img/licorne.png" class="m-10" />
 
-    <t-button
-        label="Start Game"
-        class="m-10"
-    />
+    <form
+        id="app"
+        @submit="checkForm"
+        action="#"
+        method="post"
+    >
+
+      <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <input
+            id="gameName"
+            v-model="gameName"
+            type="text"
+            name="gameName"
+            placeholder="Game Name"
+        />
+      </p>
+      <p>
+        <input
+            id="pseudo"
+            v-model="pseudo"
+            type="text"
+            name="pseudo"
+            placeholder="Your Name"
+        />
+      </p>
+
+      <p>
+        <label for="nbPlayers">Players max : </label>
+        <input
+            id="nbPlayers"
+            v-model="nbPlayers"
+            type="number"
+            name="nbPlayers"
+            min="0"
+        />
+      </p>
+      <p>
+        <button class="button" type="submit" value="Submit" >Submit</button>
+      </p>
+    </form>
   </div>
 </template>
 
 <script>
-import TButton from "@/components/tools/TButton.vue";
-
 export default {
   name: "Create",
-  components: {
-    TButton
+  data: () => {
+    return {
+      errors: [],
+      gameName: null,
+      nbPlayers: null,
+      pseudo: null,
+    };
+  },
+  methods:{
+    checkForm: function (e) {
+      if (this.name && this.age) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.gameName) {
+        this.errors.push('Game name required.');
+      }
+      if (!this.pseudo) {
+        this.errors.push('Your name is required');
+      }
+      if (!this.nbPlayers) {
+        this.errors.push('N required.');
+      }
+      e.preventDefault();
+    }
   }
-}
+};
 </script>
 
 <style scoped>
 
+.button {
+  height: 25px;
+  width: fit-content;
+  display: flex;
+  background-color: #00F5D4;
+  color: black;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2em;
+  font-family: inherit;
+  padding: 10px;
+  border: 0;
+  border-radius: 10px;
+}
 </style>
