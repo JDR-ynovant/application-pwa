3a48d1a97cdcc3b55bbf0508a2e984f9080fa479
Maxime Valette <valettemaxime26@gmail.com>
2021-06-03 15:37:21 +0200

    intialisation du menu du jeu

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
-    <div class="menu-container">
-      <t-button label="Retour" class="m-10" @click="$router.go(-1)" />
-    </div>
+    <section class="menu-container">
+      <t-menu-item label="🔔" class="m-10" @click="openMenuContent('historique')"/>
+      <t-menu-item label="🧭" class="m-10" @click="openMenuContent('carte')"/>
+      <t-menu-item label="🏠" routeName="Home" class="m-10"/>
+    </section>
     <div class="grid-container">
       <grid :nbRows="36" :nbCols="36"></grid>
     </div>
@@ -13,16 +15,32 @@
 // @ is an alias to /src
 import Grid from "@/components/grids/Grid.vue";
 import TButton from "@/components/tools/TButton.vue";
+import TMenuItem from "@/components/tools/TMenuItem.vue";
 
 export default {
   name: "Home",
   components: {
     Grid,
     TButton,
+    TMenuItem,
   },
+  methods: {
+    openMenuContent (name) {
+      console.log(name);
+    }
+  }
 };
 </script>
 <style scoped>
+.menu-container {
+  position: fixed;
+  float: left;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-around;
+}
+
 .grid-container {
   display: flex;
   align-items: center;
