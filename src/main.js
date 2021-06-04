7573b87e4de4cf4fbec6d219c4ab50a15a82c013
Maxime Valette <valettemaxime26@gmail.com>
2021-06-04 14:12:12 +0200

    quick fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "./assets/app.scss";
 
 Vue.config.productionTip = false;
 
