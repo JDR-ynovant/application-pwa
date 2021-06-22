073083ad6ac2690d0dd7d14d4662c5937114d883
Maxime Valette <valettemaxime26@gmail.com>
2021-06-22 17:24:20 +0200

    fix service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,31 +2,53 @@
 
 import { register } from "register-service-worker";
 
+function subscribe() {
+  navigator.serviceWorker.ready
+    .then(function(registration) {
+      const vapidPublicKey = 'BJfEHGrPVwd1KGexvxv7fzlGi7eV2au_kZNrfvpMz73vDBH24duEvaIGKvhCH_28hPJ47ueX_bdxGk3kUNk3kv0';
+
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+      });
+    })
+    .then(function(subscription) {
+      console.log(
+        JSON.stringify({
+          subscription: subscription,
+        })
+      );
+    })
+    .catch(err => console.error(err));
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+}
+
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
+    async ready(registration) {
+      console.log(registration);
+      const subscription = await registration.pushManager.getSubscription();
+      if (!subscription) {
+        subscribe();
+      } else {
+        console.log(
+          JSON.stringify({
+            subscription: subscription,
+          })
+        );
+      }
       console.log(
         "App is being served from cache by a service worker.\n" +
           "For more details, visit https://goo.gl/AFskqB"
       );
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("service-worker.js");
-        navigator.serviceWorker.ready
-          .then(function (registration) {
-            return registration.pushManager.getSubscription();
-          })
-          .then(function (subscription) {
-            if (!subscription) {
-              subscribe();
-            } else {
-              console.log(
-                JSON.stringify({
-                  subscription: subscription,
-                })
-              );
-            }
-          });
-      }
     },
     registered() {
       console.log("Service worker has been registered.");
