7f7ee9fdbd6359ec5572b762f0720d602d2efcf0
mguerrero <marianne.guerrero5@orange.fr>
2021-06-22 16:44:08 +0200

    plugin vuexpersistence ok

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-//import VuexPersistence from "vuex-persist";
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex);
 
@@ -63,5 +63,5 @@ export default new Vuex.Store({
     },
   },
   modules: {},
-  //plugins: [vuexPersist.plugin],
+  plugins: [new VuexPersistence().plugin]
 });
