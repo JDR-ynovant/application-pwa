645b566576ac9304b8cfbb8a5302783034f7d89f
Maxime Valette <valettemaxime26@gmail.com>
2021-06-23 15:35:10 +0200

    fix asset version

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   lintOnSave: false,
   pwa: {
     name: "CandyFight",
-    assetsVersion: "0.6",
+    assetsVersion: "0.7",
     themeColor: "#ffb0b0",
     appleMobileWebAppCapable: "yes",
     manifestOptions: {
